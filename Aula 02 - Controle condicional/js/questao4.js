18f567dc2bb9b99d710e341a8f4b0d20930a6342
liasarahgrude-rgb <liasarahgrude@gmail.com>
2026-08-10 11:43:01 -0300

    Organizando modulo

---
+++ Aula 02 - Controle condicional/js/questao4.js
@@ -0,0 +1,3 @@
+// Crie um mini projeto que usa prompt() para perguntar a idade do usuário. 
+// O programa deve primeiro validar se o input é um número válido e positivo. 
+// Se for válido, deve usar um if/else para informar se o usuário tem 18 anos ou mais (Acesso Liberado) ou menos (Acesso Negado).
