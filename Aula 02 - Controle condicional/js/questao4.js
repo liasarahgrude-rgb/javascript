fd9f1178bab0eecf191e7641461acf07234bdc03
liasarahgrude-rgb <liasarahgrude@gmail.com>
2026-08-11 17:53:08 -0300

    Aula looping while

--- Aula 02 - Controle condicional/js/questao4.js
+++ Aula 02 - Controle condicional/js/questao4.js
@@ -1,3 +1,14 @@
 // Crie um mini projeto que usa prompt() para perguntar a idade do usuário. 
 // O programa deve primeiro validar se o input é um número válido e positivo. 
 // Se for válido, deve usar um if/else para informar se o usuário tem 18 anos ou mais (Acesso Liberado) ou menos (Acesso Negado).
+let idade = parseInt(prompt("Digite sua idade:"))
+
+if (idade != "" && idade > 0){
+    if(idade >= 18){
+        console.log("Acesso liberado")
+    }else{
+        console.log("Acesso negado")
+    }
+}else{
+    console.log("Erro")
+}
