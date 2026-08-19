2e15505ab281d69cb88aa9e92736c6516973b987
liasarahgrude-rgb <liasarahgrude@gmail.com>
2026-08-19 18:28:02 -0300

    Adicionando exercicio

--- Aula 04 - For loop/js/questao4.js
+++ Aula 04 - For loop/js/questao4.js
@@ -3,3 +3,9 @@
 // Simule 4 fases (repetições) do jogo, onde cada fase concede 50 pontos de XP. 
 // Use um laço for para realizar as repetições e acumular o XP total. 
 // Ao final do laço, imprima o total de XP.
+let xp_total = 0;
+for (let i = 1; i<= 4; i++){
+    let xp_ganho = Number(prompt("Digite o valor de XP que você ganhou na" + fase + "ª fase:"));
+    xp_total += xp_ganho;
+}
+console.log(`XP Total acumulado: ${xp_total} 🏆`);
